bf2b5d57cbebb79c165b2ed82b95210fab863d12
Oliver Siccha <oliver.siccha@d-fine.de>
2021-11-16 14:02:48 +0100

    add line splitting to better fit maximum capacity constraint

--- DockerInput/Backends/DwaveBackends.py
+++ DockerInput/Backends/DwaveBackends.py
@@ -13,7 +13,7 @@ from dwave.system import LeapHybridSampler
 from dwave.system import DWaveSampler, EmbeddingComposite
 
 #import pandas
-
+from numpy import round as npround 
 
 class DwaveTabuSampler(BackendBase):
     def __init__(self):
@@ -21,7 +21,20 @@ class DwaveTabuSampler(BackendBase):
         self.metaInfo = {}
 
     def processSolution(self, network, transformedProblem, solution):
-        return solution
+        bestSample = self.choose_sample(solution, self.network, strategy=self.strategy)
+        solutionState = [
+            id for id, value in bestSample.items() if value == -1
+        ]
+        lineValues = transformedProblem[0].getLineValues(solutionState)
+        indCostCon = transformedProblem[0].individualCostContribution(solutionState)
+        totCost = transformedProblem[0].calcCost(solutionState)
+        resultDict = {
+                'solutionState' : solutionState,
+                'lineValues' : lineValues,
+                'individualCostContribution' : indCostCon,
+                'totalCost' : totCost,
+        }
+        return resultDict
 
     def transformProblemForOptimizer(self, network):
         print("transforming Problem...")
@@ -61,22 +74,61 @@ class DwaveTabuSampler(BackendBase):
             result += network.generators_t['p_max_pu'].loc[snapshot].iloc[index]
         return result
 
+
     @staticmethod
-    def choose_sample(solution, network):
-        return solution.first.sample
+    def choose_sample(solution, network, strategy="LowestEnergy", snapshot=None):
+    
+        if strategy == 'LowestEnergy':
+            return solution.first.sample
+
+        if snapshot is None:
+            snapshot = network.snapshots[0]
+
+        df = solution.to_pandas_dataframe()
+        print(f"INDEX LOWEST E: {df['energy'].idxmin()}\n")
+
+        if strategy == 'MajorityVote':
+            return df.mode().iloc[0]
+
+        if strategy == 'ClosestSample':
+            total_load = network.loads_t['p_set'].loc[snapshot].sum()
+            df['deviation_from_opt_load'] = df.apply(
+                    lambda row: abs(total_load -
+                                    DwaveTabuSampler.power_output(
+                                        network,
+                                        [id for id, value in row.items() if value == -1],
+                                        snapshot
+                                        )
+                                    ),
+                    axis=1
+            )
+            min_deviation = df['deviation_from_opt_load'].min()
+            ClosestSamples = df[df['deviation_from_opt_load'] == min_deviation]
+            print(f"INDEX OF CHOSEN SAMPLE: {ClosestSamples['energy'].idxmin()}")
+            result_row = ClosestSamples.loc[ClosestSamples['energy'].idxmin()]
+            return result_row[:-3]
+
+        raise ValueError("The chosen strategy for picking a sample is not supported")
+
+#    @staticmethod
+#    def choose_sample(solution, network):
+#        return solution.first.sample
 
     @classmethod
     def transformSolutionToNetwork(cls, network, transformedProblem, solution):
-        bestSample = cls.choose_sample(solution, network, strategy='LowestEnergy')
-        solutionState = [
-            id for id, value in bestSample.items() if value == -1
-        ]
+#        bestSample = cls.choose_sample(solution, network, strategy='LowestEnergy')
+#        solutionState = [
+#            id for id, value in bestSample.items() if value == -1
+#        ]
+
+        solutionState = solution['solutionState']
+        
         print("done")
         print(solutionState)
-        print(transformedProblem[0].getLineValues(solutionState))
-        print(transformedProblem[0].individualCostContribution(solutionState))
+        print(solution['lineValues'])
+        print(solution['individualCostContribution'])
         print(
-            f"Total cost (with constant terms): {transformedProblem[0].calcCost(solutionState)}"
+            f"Total cost (with constant terms): {solution['totalCost']}"
         )
         for snapshot in network.snapshots:
             power = DwaveTabuSampler.power_output(network,
@@ -170,46 +222,83 @@ class DwaveCloudDirectQPU(DwaveCloud):
                 for idx in network.snapshots
         } 
         self.generators_t = network.generators_t['p_max_pu']
-        self.network = network
-        return super().transformProblemForOptimizer(network)
+        self.network = network.copy()
 
-    @staticmethod
-    def choose_sample(solution, network, strategy="ClosestSample", snapshot=None):
-        if snapshot is None:
-            snapshot = network.snapshots[0]
-        total_load = network.loads_t['p_set'].loc[snapshot].sum()
-        df = solution.to_pandas_dataframe()
-        #pandas.set_option('display.max_columns', 40)
-        print(f"INDEX LOWEST E: {df['energy'].idxmin()}\n")
+        self.lineDictionary = {}
+        self.lineNames = network.lines.index
 
-        if strategy == 'MajorityVote':
-            print(f"using {strategy}")
-            return df.mode().iloc[0]
-    
-        if strategy == 'LowestEnergy':
-            return solution.first.sample
+        for line in self.lineNames:
+            k, v = self.splitLine(line, self.network)
+            self.lineDictionary[k]=v
 
-        if strategy == 'ClosestSample':
-            df['deviation_from_opt_load'] = df.apply(
-                    lambda row: abs(total_load -
-                                    DwaveTabuSampler.power_output(
-                                        network,
-                                        [id for id, value in row.items() if value == -1],
-                                        snapshot
-                                        )
-                                    ),
-                    axis=1
+        return super().transformProblemForOptimizer(self.network)
+
+    def splitLine(self, line, network):
+        """
+        splits up a line into multiple lines such that each new line
+        has capacity 2^n - 1 for some n. Modifies the network given
+        as an argument and returns the original line name and how
+        many components where used to split it up
+        """
+        remaining_s_nom = network.lines.loc[line].s_nom
+        componentNames = []
+        numComponents = 0
+        while remaining_s_nom > 0:
+            binLength = len("{0:b}".format(int(npround(remaining_s_nom))))-1
+            if binLength == 0:
+                binLength += 1
+            magnitude = 2 ** binLength - 1
+            newName = f"{line}_split_{numComponents}"
+            remaining_s_nom -= magnitude
+            componentNames.append(newName)
+            network.add(
+                "Line",
+                newName,
+                bus0=network.lines.loc[line].bus0,
+                bus1=network.lines.loc[line].bus1,
+                s_nom=magnitude
             )
-            min_deviation = df['deviation_from_opt_load'].min()
-            ClosestSamples = df[df['deviation_from_opt_load'] == min_deviation]
-            print(f"INDEX OF CHOSEN SAMPLE: {closest_samples['energy'].idxmin()}")
-            result_row = ClosestSamples.loc[ClosestSamples['energy'].idxmin()]
-            return result_row[:-3]
+            numComponents += 1
+        network.remove("Line",line)
+        return (line, numComponents)
 
-        raise ValueError("The chosen strategy for picking a sample is not supported")
+    def mergeLines(self, lineValues, snapshots):
+        result = {}
+        for line, numSplits in self.lineDictionary.items():
+            for snapshot in range(len(snapshots)):
+                newKey = (line, snapshot)
+                value = 0
+                for i in range(numSplits):
+                    splitLineKey = line + "_split_" + str(i)
+                    value += lineValues[(splitLineKey, snapshot)]
+                result[newKey] = value
+        return result
+    
+#        lineValues = transformedProblem[0].getLineValues(solutionState)
+#        indCostCon = transformedProblem[0].individualCostContribution(solutionState)
+#        totCost = transformedProblem[0].calcCost(solutionState)
+#        resultDict = {
+#                'solutionState' : solutionState,
+#                'lineValues' : lineValues,
+#                'individualCostContribution' : indCostCon,
+#                'totalCost' : totCost,
 
-    def processSolution(self, network, transformedProblem, solution):
-        return solution
+    def processSolution(self, network, transformedProblem, solution, sample=None):
+        bestSample = self.choose_sample(solution, self.network)
+        resultDict = super().processSolution(
+                network, 
+                transformedProblem, 
+                solution,
+                )
+
+        lineValues = self.mergeLines(resultDict['lineValues'], network.snapshots)
+        resultDict['lineValues'] = lineValues
+
+        # optimize flow using classical greedy algorithms
+
+
+
+        return resultDict
 
     def optimize(self, transformedProblem):
         print("optimize")
@@ -226,13 +315,6 @@ class DwaveCloudDirectQPU(DwaveCloud):
                 break
             time.sleep(2)
 
-        bestSample = sampleset.first
-        generatorState = [
-            id for id, value in bestSample.sample.items() 
-                if value == -1 and id < self.num_generators
-        ]
-
-        self.metaInfo["loads"] = self.loads
         self.metaInfo["serial"] = sampleset.to_serializable()
 
         return sampleset
