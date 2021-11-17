65cac1347682851aaaff9892c90bc3a0740b39bd
Oliver Siccha <oliver.siccha@d-fine.de>
2021-11-17 09:56:13 +0100

    add inital powerflow optimisation to process dwave results

--- DockerInput/Backends/DwaveBackends.py
+++ DockerInput/Backends/DwaveBackends.py
@@ -12,8 +12,12 @@ from .IsingPypsaInterface import IsingPypsaInterface
 from dwave.system import LeapHybridSampler
 from dwave.system import DWaveSampler, EmbeddingComposite
 
-#import pandas
 from numpy import round as npround 
+import networkx as nx
+from networkx.algorithms.flow import edmonds_karp
+from networkx.algorithms.flow import build_residual_network
+from networkx.classes.function import set_edge_attributes
+
 
 class DwaveTabuSampler(BackendBase):
     def __init__(self):
@@ -86,6 +90,9 @@ class DwaveTabuSampler(BackendBase):
 
         df = solution.to_pandas_dataframe()
         print(f"INDEX LOWEST E: {df['energy'].idxmin()}\n")
+        print(df.head())
+        print("...")
+        print(df.iloc[df['energy'].idxmin()])
 
         if strategy == 'MajorityVote':
             return df.mode().iloc[0]
@@ -224,9 +231,12 @@ class DwaveCloudDirectQPU(DwaveCloud):
         self.generators_t = network.generators_t['p_max_pu']
         self.network = network.copy()
 
+        #keys: line name in original network
+        #value: number components of capacity 2^n-1 the decomposition is made of
+        #line names in dummy network for an item (k,v) in the dictionary are
+        #"{k}_split_{i}" for 0 <= i < v
         self.lineDictionary = {}
         self.lineNames = network.lines.index
-
         for line in self.lineNames:
             k, v = self.splitLine(line, self.network)
             self.lineDictionary[k]=v
@@ -238,22 +248,21 @@ class DwaveCloudDirectQPU(DwaveCloud):
         splits up a line into multiple lines such that each new line
         has capacity 2^n - 1 for some n. Modifies the network given
         as an argument and returns the original line name and how
-        many components where used to split it up
+        many components where used to split it up. Use it on the
+        network stored in self.
         """
         remaining_s_nom = network.lines.loc[line].s_nom
-        componentNames = []
         numComponents = 0
         while remaining_s_nom > 0:
             binLength = len("{0:b}".format(int(npround(remaining_s_nom))))-1
-            if binLength == 0:
+            #case remaining_s_nom <= 1
+            if binLength == 0: 
                 binLength += 1
             magnitude = 2 ** binLength - 1
-            newName = f"{line}_split_{numComponents}"
             remaining_s_nom -= magnitude
-            componentNames.append(newName)
             network.add(
                 "Line",
-                newName,
+                f"{line}_split_{numComponents}",
                 bus0=network.lines.loc[line].bus0,
                 bus1=network.lines.loc[line].bus1,
                 s_nom=magnitude
@@ -284,7 +293,6 @@ class DwaveCloudDirectQPU(DwaveCloud):
 #                'totalCost' : totCost,
 
     def processSolution(self, network, transformedProblem, solution, sample=None):
-        bestSample = self.choose_sample(solution, self.network)
         resultDict = super().processSolution(
                 network, 
                 transformedProblem, 
@@ -294,11 +302,107 @@ class DwaveCloudDirectQPU(DwaveCloud):
         lineValues = self.mergeLines(resultDict['lineValues'], network.snapshots)
         resultDict['lineValues'] = lineValues
 
-        # optimize flow using classical greedy algorithms
+        # optimize flow using classical algorithms
 
+        G,R = self.buildFlowproblem(network,
+                resultDict['solutionState'],
+                lineValues)
 
+        tic = time.perf_counter()
+        Other = edmonds_karp(G, "superSource", "superSink")
+        self.metaInfo["max_flow_time_without_quantum_start"] = time.perf_counter()-tic
+        print(Other.edges(data=True))
+
+#        Opt = edmonds_karp(G, "superSource", "superSink", residual = R)
+#        print("Opt")
+#        print(Opt.edges(data=True))
 
         return resultDict
+
+    def powerAtBus(self, network, generatorState, bus):
+        activeGeneratorIndex = [idx for idx in generatorState if idx < self.num_generators]
+        activeGenerators = network.generators_t['p_max_pu'].iloc[0].iloc[activeGeneratorIndex]
+        return activeGenerators[network.generators.loc[activeGenerators.index].bus == bus].sum()
+
+
+    # the quantum computation struggles with finetuning powerflow to match
+    # demand exactly due to flipping single bits corresponding to a large 
+    # change in power. we hope that using a classical approach to tune is
+    # practically free due to our solution already being very close to the
+    # global minimum
+    def buildFlowproblem(self, network, generatorState, lineValues):
+        """
+        build a networkx model to further optimise power flow with a residual
+        network corresponding to the quantum solution.
+        """
+
+        # turn pypsa network in nx.DiGraph. Power generation and consumption
+        # is modeled by adjusting capacity of the edge to a super source/super sink 
+        G = nx.DiGraph()
+        G.add_nodes_from(network.buses.index)
+        G.add_nodes_from(["superSource","superSink"])
+
+        for line in network.lines.index:
+            bus0 = network.lines.loc[line].bus0
+            bus1 = network.lines.loc[line].bus1
+            cap = network.lines.loc[line].s_nom
+            # network has two lines between buses, make sure not to 
+            # erase the capacity of previous line
+            if G.has_edge(bus0,bus1):
+                G[bus0][bus1]['capacity'] += cap
+                G[bus1][bus0]['capacity'] += cap
+            else:
+                G.add_edges_from([(bus0,bus1,{'capacity': cap}),
+                                (bus1,bus0,{'capacity': cap})])
+
+        for bus in network.buses.index:
+            G.add_edge(
+                "superSource",
+                bus,
+                capacity=self.powerAtBus(network, generatorState, bus)
+            )
+
+        for load in network.loads.index:
+            G.add_edge(
+                network.loads.loc[load].bus,
+                "superSink",
+                capacity=network.loads_t['p_set'].iloc[0][load],
+            )
+        # done building nx.DiGrpah
+
+        R = build_residual_network(G,capacity=511)
+        set_edge_attributes(R,0,'flow')
+
+        # generate flow for network lines
+        for line in network.lines.index:
+            bus0 = network.lines.loc[line].bus0
+            bus1 = network.lines.loc[line].bus1
+            R[bus0][bus1]['flow'] += lineValues[(line,0)]
+            R[bus1][bus0]['flow'] -= lineValues[(line,0)]
+
+        # adjust source and sink flow to make it a valid residual network
+        # edges to source/sink are not at full capacity iff there is a net
+        # demand/power generated after subtraction power flow at that bus
+        for bus in network.buses.index:
+            generatedPower = self.powerAtBus(network, generatorState, bus)
+            loadName = network.loads.index[network.loads.bus == bus][0]
+            load = network.loads_t['p_set'].iloc[0][loadName]
+            netFlowThroughBus = 0
+            for line in network.lines.index[network.lines.bus0 == bus]:
+                netFlowThroughBus += lineValues[(line,0)]
+            for line in network.lines.index[network.lines.bus1 == bus]:
+                netFlowThroughBus -= lineValues[(line,0)]
+            netPower = generatedPower \
+                    - load \
+                    + netFlowThroughBus
+
+            R["superSource"][bus]['flow'] = min(generatedPower,generatedPower-netPower)
+            R[bus]["superSource"]['flow'] = -R["superSource"][bus]['flow']
+            R[bus]["superSink"]['flow'] = min(load,load + netPower)
+            R["superSink"][bus]['flow'] = -R[bus]["superSink"]['flow']
+
+        return G,R
+
 
     def optimize(self, transformedProblem):
         print("optimize")
