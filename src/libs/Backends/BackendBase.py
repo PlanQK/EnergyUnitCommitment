a93d00abce36b71607c372af86bca57ed7ba4a62
osiccha <oliver.siccha@d-fine.de>
2022-05-02 09:20:14 +0200

    reorganize how output dicitionary is set up, add comments

--- src/libs/Backends/BackendBase.py
+++ src/libs/Backends/BackendBase.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 class BackendBase(abc.ABC):
     def __init__(self, reader: InputReader):
+        self.reader = reader
         self.network = reader.getNetwork()
         self.networkName = reader.getNetworkName()
         self.config = reader.getConfig()
@@ -38,35 +39,55 @@ class BackendBase(abc.ABC):
     def getConfig(self) -> dict:
         return self.config
 
+    def getTime(self) -> str:
+        now = datetime.today()
+        return f"{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}-{now.second}"
+
     def getOutput(self) -> dict:
         self.output["end_time"] = self.getTime()
         return self.output
 
     def setupOutputDict(self):
-        self.output = {"start_time": None,
-                       "end_time": None,
-                       "file_name": None,
-                       "config": {"Backend": self.config["Backend"],
-                                  "IsingInterface": self.config["IsingInterface"]},
-                       "components": {},
-                       "network": {},
-                       "results": {}}
-
-        if self.config["Backend"] in ["dwave-tabu", "dwave-greedy", "dwave-hybrid", "dwave-qpu", "dwave-read-qpu"]:
-            self.output["config"]["DWaveBackend"] = self.config["DWaveBackend"]
-        elif self.config["Backend"] in ["pypsa-glpk", "pypsa-fico"]:
-            self.output["config"]["PypsaBackend"] = self.config["PypsaBackend"]
-        elif self.config["Backend"] in ["sqa", "classical"]:
-            self.output["config"]["SqaBackend"] = self.config["SqaBackend"]
-        elif self.config["Backend"] in ["qaoa"]:
-            self.output["config"]["QaoaBackend"] = self.config["QaoaBackend"]
-
-        self.output["start_time"] = self.getTime()
-        self.output["file_name"] = self.networkName + "_" + self.config["Backend"] + "_" + \
-                                   self.output["start_time"] + ".json"
-
-    def getTime(self) -> str:
-        now = datetime.today()
-        dateTimeStr = f"{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}-{now.second}"
-
-        return dateTimeStr
+        """
+        creates an 'output' attribute in self in which to save results and configuration
+        data. The config entry is another dictionary with 3 keys: 'Backend' has config
+        data that all backends share, 'IsingInterface' has config data of the class
+        used to convert a unit commitment problem into an ising spin problem
+        and a key named in `BackendToSolver` for backend specific configurations
+        
+        Returns:
+            (None) (over)writes the attribute `output` with a dicitionary containing
+            configuration data and empty fields to insert results into later on.
+        """
+        startTime = self.getTime()
+        # dictionary which solver has which backend specific extra data where. Keys are
+        # broader categories of backends and values are a list of solvers that use that
+        # key to store additional config info
+        # TODO flatten structure, by making new key, backend category and a generic key
+        ## for additional configs for this category of solvers
+        BackendToSolver = {
+                "DWaveBackend": ["dwave-tabu", "dwave-greedy", "dwave-hybrid", "dwave-qpu", "dwave-read-qpu"],
+                "PypsaBackend": ["pypsa-glpk", "pypsa-fico"],
+                "SqaBackend": ["sqa", "classical"],
+                "QaoaBackend": ["qaoa"],
+        }
+        for backend, solverList in BackendToSolver.items():
+            if self.config["Backend"] in solverList:
+                self.output = {"start_time": startTime,
+                               "end_time": None,
+                               "file_name":  "_".join([
+                                        self.networkName,
+                                        self.config["Backend"],
+                                        startTime + ".json"
+                                        ]),
+                               "config": {
+                                        "Backend": self.config["Backend"],
+                                        "IsingInterface": self.config["IsingInterface"],
+                                        backend: self.config[backend],
+                                      },
+                               "components": {},
+                               "network": {},
+                               "results": {},
+                               }
+                return
+                    
