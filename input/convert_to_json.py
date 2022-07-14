e1ce51c8a32f302e97fa3a0e223f3ee009f00c83
osiccha <oliver.siccha@d-fine.de>
2022-07-14 09:28:10 +0200

    add doc string and default save name is none is passed

--- input/convert_to_json.py
+++ input/convert_to_json.py
@@ -10,17 +10,37 @@ import pypsa
 import sys
 import os
 
+usage_string = """
+This takes up to two arguments, one for the name of the file to be loaded, and one
+for the name of the file the json gets dumped to. Ommitting the last parameter will
+set the save file name to the same name as the input but changing it to json
+
+networks are assumed to be in the folder sweetNetworks/
+config files are assumed to be in the folder src/Configs/
+
+convert_to_json some_network_or_config_with_ending save_file_without_json
+                some_network_or_config_with_ending
+"""
 
 def main():
+    if len(sys.argv) == 1:
+        print(usage_string)
+        return
+    
     # make a prefix so all paths start from git root. 
     if os.path.split(__file__)[0] == 'input':
-        path_prefix = "./"
-    else:
         path_prefix = "../"
+    else:
+        path_prefix = "./"
 
     input_file = sys.argv[1]
     filetype = input_file.split(".")[-1]
-    output_file = path_prefix + "input/" + sys.argv[2] + ".json"
+    if len(sys.argv) >= 3:
+        output_name = sys.argv[2]
+    else:
+        output_name = input_file[: -len(filetype)]
+
+    output_file = path_prefix + "input/" + output_name + ".json"
 
     print(f"reading {input_file}")
     if filetype == "yaml":
