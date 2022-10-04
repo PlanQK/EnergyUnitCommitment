0129c6b173e3390c99a2332386a9463115444244
Oliver Siccha <oliver.siccha@d-fine.de>
2022-10-04 11:30:50 +0200

    update example plot script

--- scripts/make_plots_example.py
+++ scripts/make_plots_example.py
@@ -22,43 +22,46 @@ You can then run the script `make_plots.py` by either running it directly, or ma
 from plot_results import *
 
 
-def __main__():
+def main():
     plt.style.use("seaborn")
-
-    networkbatch = "infoNocost_220124cost5input_"
 
     # In order to search the result folders, the plot_agent requires a dictionary when instantiating it.
     # The keys denote which solver result folder will be searched and the value is a list of glob expressions
     # that will be used in the corresponding result folder of the solver
-    # The constraints argument is a dictionary. The result files will be filtered such that if a key of the constraints
+    # The constraints argument is a dictionary. The keys are names of result parameters and the values
+    # are lists of parameter values. The result files will be filtered such that if the key of the constraints
     # is also the in the json of the result, the value in that json has to be in the value of that key.
+
+    # The following code block searches all sqa results for files containing the `defaultnetwork.nc`
+    # network in it's name with any additional ending. All sqa results which weren't run with the trotter
+    # slices config config of either 100, 150 or 200 will be discarded.
+    networkbatch = "defaultnetwork.nc"
     plot_agent = PlottingAgent.extract_from_json({
-        "sqa": [networkbatch + "*[36592]0_?_20.nc*"],
-        "pypsa_glpk": [networkbatch + "*[36592]0_?_20.nc*"],
+        "sqa": [networkbatch +"*"],
         },
-        constraints={
-            "problemSize": [30, 60, 90, 120, 150],
-            "trotterSlices": [500],
-        })
+        # constraints={"trotter_slices": [100,150,200]}
+        )
 
-    plot_agent.make_figure("large_problem_sqa_glpk",
-                           split_fields=["solver"],
-                           )
-    plot_agent.make_figure("large_problem_time_sqa_glpk",
-                           y_field_list=["optimization_time"],
-                           split_fields=["solver"],
-                           )
-    plot_agent.make_figure("large_problem_time_and_cost_sqa_glpk",
-                           y_field_list=["optimization_time", "total_cost"],
-                           split_fields=["solver"],
-                           )
+    # This will create a plot nammed "first_figure" with the trotter slices on the x-axis and
+    # two lines: one for the average kirchhoff cost and one for the average marginal costs
+    # The empty `split_fields` means that the results aren't grouped by the value of a third
+    # parameter
+    plot_agent.make_figure("first_figure",
+                            x_field = "trotter_slices",
+                            y_field_list = ["kirchhoff_cost", "marginal_cost"],
+                            split_fields = []
+                            )
 
-    # dump pandas dataframe as csv with git root as current directory
+    # This dump the underlying pandas DataFrame as a csv file with the git root as the current directory
     plot_agent.to_csv("results_csv/some_results.csv")
-    # remake plot_agent with the same data set
-    plot_agent.read_csv("some_results.csv")
 
-    # Add additional plots
+    # We can remake a plot_agent with the same data set by reading the dumped csv
+    plot_agend = plot_agent.read_csv("results_csv/some_results.csv")
+    plot_agent.make_figure("same_figure_from_same_csv",
+                            x_field = "trotter_slices",
+                            y_field_list = ["kirchhoff_cost", "marginal_cost"],
+                            split_fields = []
+                            )
     return
 
 
