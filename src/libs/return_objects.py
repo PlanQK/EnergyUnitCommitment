dcd0b90a0cb31501a4df038c4ec1607850ae05ce
Oliver Siccha <oliver.siccha@d-fine.de>
2022-11-09 11:28:55 +0100

    clean up doc strings and typing

--- src/libs/return_objects.py
+++ src/libs/return_objects.py
@@ -13,22 +13,22 @@ class Response:
     A class for wrapping the serialization of optimization results.
     """
 
-    def to_json(self):
+    def to_json(self) -> str:
         """
         Converts the data of the response into json format and returns it as a string
-    
+
         Returns:
             (str) The serialized response as a string
         """
         return json.dumps(self, default=lambda o: getattr(o, '__dict__', str(o)), sort_keys=True, indent=2)
 
     @abstractmethod
-    def dump_results(self, folder: str = "problemset/"):
+    def dump_results(self, folder: str = "problemset/") -> None:
         """
         An abstract method to be overwritten in child classes. The behaviour of the method
         is to write the contents of the response to the disk. This is needed for saving the 
         results of an optimization if you ran it locally in a docker container
-    
+
         Args:
             folder: (str) location where the information of the response is dumped. The default
                 option is the mount point that the makefile uses for mounting the network folder
@@ -40,7 +40,7 @@ class Response:
 
 class ErrorResponse(Response):
     """
-    Represents an error to be passed back to the caller.
+    Represents an error thrown during the optimization to be passed back to the caller.
 
     Args:
         code (str): HTTP status code to be passed back to the caller
@@ -51,15 +51,16 @@ class ErrorResponse(Response):
         self.code = code
         self.detail = detail
 
-    def dump_results(self, folder: str = "problemset/"):
+    def dump_results(self, folder: str = "problemset/") -> None:
         """
-        saves the thrown error to the disk. Instead of using the name specified
+        Saves the thrown error to the disk. Instead of using the name specified
         in the config for the result, this will instead use a name using the
         time when the error was encountered and which error code was thrown.
-    
+
         Args:
             folder: (str) the location where to save the response. The default
                 path is the mount point of the makefile for the networks
+
         Returns:
             (None) Saves the error code to the mount point of the networks
         """
@@ -73,7 +74,7 @@ class ErrorResponse(Response):
 
 class ResultResponse(Response):
     """
-    Represents the result to be passed back to the caller.
+    Represents the result of a successful optimization to be passed back to the caller.
 
     Args:
         result (dict): serializable dictionary to be passed back to the caller
@@ -84,14 +85,15 @@ class ResultResponse(Response):
         self.result = result
         self.metadata = metadata
 
-    def dump_results(self, folder: str = "problemset/"):
+    def dump_results(self, folder: str = "problemset/") -> None:
         """
         Saves the result of the local optimization in a docker container.
-    
+
         Args:
             folder: (str) 
                 The location where to save the response. The default
                 path is the mount point of the makefile for the networks
+
         Returns:
             (None) Saves the optimization result to the mount point of the networks
         """
